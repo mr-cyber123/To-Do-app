7ce02fe2dd3340a83454dcb46be8caa47209df85
niyas <mdniyas@gmail.com>
2022-03-31 23:48:13 +0530

    make it better

---
+++ public/browser.js
@@ -0,0 +1,27 @@
+document.addEventListener("click",function(e){
+    // update
+    if(e.target.classList.contains("edit-me")){
+       let res= prompt("enter the new value",e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+
+       
+       axios.post('/update-item',{text:res,id:e.target.getAttribute("data-id")}).then(function(){
+           e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = res
+       }).catch(function(){
+           console.log("error try now")
+       })
+    }
+
+    // delete
+
+    if(e.target.classList.contains("delete-me")){ 
+        confirm("do you want to delete")
+ 
+        
+        axios.post('/delete-item',{id:e.target.getAttribute("data-id")}).then(function(){
+            e.target.parentElement.parentElement.remove()
+        }).catch(function(){
+            console.log("error try now")
+        })
+     }
+
+})
